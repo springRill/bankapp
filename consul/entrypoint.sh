092dceb0ba5ea8e6d4c87717fb944b3dfe527682
springRill <springRill@gmail.com>
2025-08-11 18:56:21 +0300

    docker-compose.yml

--- consul/entrypoint.sh
+++ consul/entrypoint.sh
@@ -1,22 +1,30 @@
 #!/bin/sh
-
-# Завершаем работу, если какая-либо команда не выполнится
 set -e
 
-# Ждем, пока Consul будет готов
-echo 'Waiting for Consul to be ready...'
-sleep 2
+# Запускаем Consul в фоне
+consul agent -dev -client=0.0.0.0 &
 
-until consul members -http-addr=consul:8500 | grep -q 'server'; do
+# PID процесса Consул
+CONSUL_PID=$!
+
+echo 'Waiting for Consul to be ready...'
+
+# Ждем, пока Consul не станет готов (проверяем endpoint)
+until curl -s http://localhost:8500/v1/agent/self | grep -q '"Config"' ; do
   echo 'Consul is not ready yet, retrying...'
   sleep 1
 done
+
 echo 'Consul is ready. Importing configuration from YAML...'
 
 # Читаем содержимое YAML-файла
 yaml_content=$(cat /config/config.yaml)
 
-# Помещаем весь YAML в Consul под одним ключом
-consul kv put -http-addr=consul:8500 'config/application/data' "$yaml_content"
+# Помещаем YAML в Consul KV
+consul kv put 'config/application/data' "$yaml_content"
 
-echo 'Импорт завершен.'
+echo 'Import finished.'
+
+# Ждем завершения процесса Consul, чтобы контейнер не завершился
+wait $CONSUL_PID
+
